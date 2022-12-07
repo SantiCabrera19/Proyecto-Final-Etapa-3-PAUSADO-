5d5fc58fafcd4087009b4bbd89df97f6957393d0
santiagocabrera19 <cabrerasanty321@gmail.com>
2022-12-07 00:48:24 -0300

    Modificacion en app.js funcional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Header from "./components/Header";
 import Pagination from "./components/Pagination";
 import FormSearch from "./components/Searcher";
@@ -13,7 +12,9 @@ function App() {
         <p className="lead text-center"></p>
         <Header/>
         <FormSearch/>
-       
+          <br></br>
+          <br></br>
+          <br></br>
         <Pagination/>
       </div>
     </div>
